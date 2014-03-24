1e095f6aa50792ce28a5067a185ff8e39da21ca0
q45 <quintinsmith@icloud.com>
2014-03-24 15:47:36 -0600

    zipcode added to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140324204936) do
+ActiveRecord::Schema.define(version: 20140324214603) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140324204936) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "cell"
+    t.integer  "zip_code"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
